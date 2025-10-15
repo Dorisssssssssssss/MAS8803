a26f2cc17fae7e6503b41ff48fe4c155998ccd4d
Dorisssssssssssss <xtao65@gatech.edu>
2025-10-14 20:14:33 -0400

    Initial Commit

---
+++ 8803NourishFit/_803NourishFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _803NourishFitApp.swift
+//  8803NourishFit
+//
+//  Created by XIN on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct _803NourishFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
